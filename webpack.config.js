e20420baf5aed81dc75360cbcfb54d7453edaaad
Will Downey <willd20@vt.edu>
2022-07-03 19:52:41 -0400

    Babel configuration

--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'inline-source-map',
-  test: /\.m?js$/,
-  exclude: /node_modules/,
-  use: {
-    loader: 'babel-loader',
-    options: {
-      presets: [
-        ['@babel/preset-env', { targets: 'defaults' }],
-      ],
-    },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
 };
